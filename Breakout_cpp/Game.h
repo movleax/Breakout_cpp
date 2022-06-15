a046fbbb746acf061244bf41733e55bd3c2ee93b
movleax <6619780+movleax@users.noreply.github.com>
2022-06-14 20:21:29 -0700

    WIP

--- Breakout_cpp/Game.h
+++ Breakout_cpp/Game.h
@@ -1,1 +1,70 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include "iGame.h"
+#include "GameState.h"
+
+using namespace sf;
+
+class Game : public iGame
+{
+	GameState* _gameState;
+	sf::RenderWindow window;
+	
+public:
+	Game()
+		: window(VideoMode(800, 600), "SFML works!")
+	{
+		// define a starting game state to load
+		// _gameState = new MainGame(...);
+	}
+	~Game()
+	{
+
+	}
+	void LoadGameState(GameState* gameState)
+	{
+
+	}
+	void CheckStateChange(GameState* gameState)
+	{
+
+	}
+	void Draw() override
+	{
+		// Call _gameState->Draw()
+	}
+	void Logic() override
+	{
+		// Call _gameState->Logic()
+	}
+	void Update() override
+	{
+		Logic();
+		CheckCollisions();
+	}
+	void CheckCollisions() override
+	{
+		// Call _gameState->CheckCollisions()
+	}
+	void Cycle() override
+	{
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			Update();
+
+			window.clear();
+			Draw();
+			window.display();
+		}
+	}
+};
+
+
--- Breakout_cpp/GameState.h
+++ Breakout_cpp/GameState.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#include "GameProxy.h"
+#include "iGame.h"
+
+
+class GameState : public iGame
+{
+protected:
+	GameProxy* gameProxy;
+	/*Input* input;
+	std::vector<GameObjects*> gameObjects;*/
+public:
+	/*GameState(const GameProxy* proxy)
+	{
+
+	}*/
+	virtual ~GameState()
+	{
+
+	}
+	/*void AddGameObject(GameObject* gameObj)
+	{
+
+	}
+	void AddInputHandler(Input* input)
+	{
+
+	}*/
+	void HandleDeactivatedObjects()
+	{
+
+	}
+	void Draw() override
+	{
+
+	}
+	void Logic() override
+	{
+
+	}
+	void Update() override
+	{
+
+	}
+	void CheckCollisions() override
+	{
+
+	}
+	void Cycle() override
+	{
+
+	}
+};
